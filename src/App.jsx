6d51328eb82970fe0f078ce448e33a58ce9cd19e
Alexis Martinez <jhonyalexis100@gmail.com>
2024-08-09 14:48:05 -0500

    Create Routes with react-router-dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ProductList from './components/ProductList';
+import Navigation from './template/Navigation';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 
 
@@ -9,7 +11,12 @@ function App() {
 
   return (
     <>
-    <ProductList/>
+    <BrowserRouter>
+    <Navigation />
+    <Routes>
+      <Route exact path='/' element={<ProductList />} />
+    </Routes>
+    </BrowserRouter>
 
     </>
   )
